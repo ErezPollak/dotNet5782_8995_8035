a8cb71af99dc9e9997a0d219670e1e0cd05a4178
mordechyCohen <mordechyc@gmail.com>
2021-10-06 18:47:27 +0300

    "I am also here" function

---
+++ ConsoleApp1/Program8035.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    partial class Program
+    {
+
+        static partial void Welcome8035()
+        {
+            Console.WriteLine("aaaaa");
+        }
+
+    }
+}
