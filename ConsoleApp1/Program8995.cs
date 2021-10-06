fc7a3d85ed2da891e61c804ad52e09bdc34a5d72
Erez Polak <pwlqrz@gmail.com>
2021-10-06 18:32:29 +0300

    initial code + welcome functions

---
+++ ConsoleApp1/Program8995.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            s = Welcome8995();
+            Welcome8035();
+        }
+
+        static partial void Welcome8035();
+
+        private static string Welcome8995()
+        {
+            string s;
+            Console.WriteLine("Enter your name: ");
+            s = Console.ReadLine();
+            Console.WriteLine(s + " welcome to my first console application");
+            return s;
+        }
+    }
+}
